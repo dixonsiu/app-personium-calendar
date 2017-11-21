c2b31dd4cb0c89c403432abebd6aedc986fb3ef6
Akio Shimono <shimono@users.noreply.github.com>
2017-11-21 15:48:00 +0900

    Create updateVEvent.js

---
+++ bar/90_contents/Engine/updateVEvent.js
@@ -0,0 +1,14 @@
+/*
+ * Create / Update / Delete a single VEvent, reflecting the result to the source service.
+ *   HTTP Method :   
+ *           POST:   Create
+ *           PUT:    Update
+ *           DELETE: Delete
+ *   HTTP Request Body :  JSON with following parameters 
+ *           
+ *           DELETE, UPDATE
+ *              __id
+ *           CREATE, UPDATE
+ *              description
+ *              ....
+ */
