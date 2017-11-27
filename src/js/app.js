7c4ac2568abd9b317830a7d70da8fbc3cd007cd2
Dixon Siu <siu.dixon@jp.fujitsu.com>
2017-11-27 17:45:22 +0900

    GUI components

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const APP_URL = "https://***/***/";
+
+getNamesapces = function() {
+    return ['common', 'glossary'];
+};
+
+additionalCallback = function() {
+    Common.setAppCellUrl();
+
+    Common.setAccessData();
+    
+    $('#dvOverlay').on('click', function() {
+        $(".overlay").removeClass('overlay-on');
+        $(".slide-menu").removeClass('slide-on');
+    });
+
+    if (!Common.checkParam()) {
+        // cannot do anything to recover
+        // display a dialog and close the app.
+        return;
+    };
+
+    Common.setIdleTime();
+
+    Common.getProfileName(Common.getCellUrl(), displayMyDisplayName);
+};
+
+displayMyDisplayName = function(extUrl, dispName) {
+    $("#dispName")
+        .attr("data-i18n", "[html]glossary:msg.info.description")
+        .localize({
+            "name": dispName
+        });
+};
