b2a29f05f8c906526cabf8e281207cc827bc608d
HiroyukiYamashima <yamashima@jp.fujitsu.com>
2017-11-28 17:06:13 +0900

    Add script that make Accessor and return status:200.

--- bar/90_contents/Engine/sync.js
+++ bar/90_contents/Engine/sync.js
@@ -1,7 +1,33 @@
 /*
  * Create VCalendar's events from the specified server.
- *  Input: 
+ *  Input:
  *     srcUrl, Id
  *     syncPeriod
  *
  */
+
+ function(request){
+
+   var collectionName = "OData";
+   var entityType = "vevent";
+
+   try {
+       var personalBoxAccessor = _p.as("client").cell(pjvm.getCellName()).box(pjvm.getBoxName());
+       var personalCollectionAccessor = personalBoxAccessor.odata(collectionName);
+       var personalEntityAccessor = personalCollectionAccessor.entitySet(entityType);
+       
+   } catch (e) {
+       return {
+         status: 500,
+         headers: { "Content-Type": "text/html" },
+         body: ["Server Error occurred. 01 : " + e]
+       };
+   }
+
+   // resを定義
+   return {
+       status: 200,
+       headers: {"Content-Type":"application/json"},
+//       body: [OK]
+   };
+ }
--- bar/90_contents/Engine/updateVEvent.js
+++ bar/90_contents/Engine/updateVEvent.js
@@ -1,14 +1,40 @@
 /*
  * Create / Update / Delete a single VEvent, reflecting the result to the source service.
- *   HTTP Method :   
+ *   HTTP Method :
  *           POST:   Create
  *           PUT:    Update
  *           DELETE: Delete
- *   HTTP Request Body :  JSON with following parameters 
- *           
+ *   HTTP Request Body :  JSON with following parameters
+ *
  *           DELETE, UPDATE
  *              __id
  *           CREATE, UPDATE
  *              description
  *              ....
  */
+
+ function(request){
+
+   var collectionName = "OData";
+   var entityType = "vevent";
+
+   try {
+       var personalBoxAccessor = _p.as("client").cell(pjvm.getCellName()).box(pjvm.getBoxName());
+       var personalCollectionAccessor = personalBoxAccessor.odata(collectionName);
+       var personalEntityAccessor = personalCollectionAccessor.entitySet(entityType);
+
+   } catch (e) {
+       return {
+         status: 500,
+         headers: { "Content-Type": "text/html" },
+         body: ["Server Error occurred. 01 : " + e]
+       };
+   }
+
+   // resを定義
+   return {
+       status: 200,
+       headers: {"Content-Type":"application/json"},
+//       body: [OK]
+   };
+ }
